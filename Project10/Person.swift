428bc66fafe170a2beac8b17e41d4bfdd52bd3bf
Victor Macari <victorm.macari@gmail.com>
2024-11-15 13:10:33 +0200

    feat: adds final version of the app

---
+++ Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Victor Macari on 13.11.2024.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project10/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Victor Macari on 13.11.2024.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
